5069cf53d4b8b07624f3047a4cd661906952e3db
Thauan-Fonseca <jeyquisson.lima@grupoboticario.com.br>
2024-02-21 10:47:32 -0300

    feat: criação da rota de autenticação

--- session-service/src/controllers/auth-ctrl.js
+++ session-service/src/controllers/auth-ctrl.js
@@ -1,6 +1,21 @@
+const jwt = require('jsonwebtoken')
+const SessionController = require('./session-ctrl');
 class AuthController {
     static auth(req, res) {
-        res.status(200).json({message: 'Auth service'});
+        const {authorization} = req.headers
+
+        const token = authorization.split(' ')[1];
+
+        try {
+            const payload = jwt.verify(token, process.env.JWT_SECRET_KEY)
+            console.log(payload)
+
+            
+        } catch (error) {
+            console.log(error)
+        }
+        
+        res.status(200).json({message: 'Session created successfully!'});
     }
 }
 
