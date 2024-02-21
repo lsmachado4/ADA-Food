2ba062f126c529293d2573a1a254c808df666de7
Thauan-Fonseca <jeyquisson.lima@grupoboticario.com.br>
2024-02-21 10:47:32 -0300

    feat: criação da rota de autenticação

--- api-gateway/src/handlers/auth-handler.js
+++ api-gateway/src/handlers/auth-handler.js
@@ -3,8 +3,22 @@ const axios = require('axios')
 
 class AuthHandler {
 static async auth(req, res) {
-    const {data} = await axios.get(`${process.env.AUTH_SERVICE_URL}/auth`)
+  try {
+    const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({error: 'Token not provided'});
+  }
+    const {data} = await axios.get(`${process.env.AUTH_SERVICE_URL}/auth`, {
+      headers: {
+        Authorization: token, // Incluindo o token no header da requisição
+      },
+    })
     res.status(200).send(data);
+    return true
+  } catch (error) {
+    console.log(error)
+  }
+  
   }
 }
 
