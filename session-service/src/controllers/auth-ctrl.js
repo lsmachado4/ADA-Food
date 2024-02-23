4ef249410c5fb9252c88b78dbc268ed4be11ce95
Thauan-Fonseca <jeyquisson.lima@grupoboticario.com.br>
2024-02-23 11:22:08 -0300

    feat: inclusion of authentication middleware and authentication service

--- session-service/src/controllers/auth-ctrl.js
+++ session-service/src/controllers/auth-ctrl.js
@@ -1,22 +1,26 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const SessionController = require('./session-ctrl');
+
 class AuthController {
     static auth(req, res) {
-        const {authorization} = req.headers
-
-        const token = authorization.split(' ')[1];
+        const {authorization} = req.headers;
+        console.log("Autorização ", authorization);
+        if (!authorization) {
+            return res.status(401).json({ error: 'Authorization header missing' });
+          }
+          const [, token] = authorization.split(' ')
 
         try {
-            const payload = jwt.verify(token, process.env.JWT_SECRET_KEY)
-            console.log(payload)
-
-            
+            const payload = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            console.log("payload", payload);
+            const name = payload.name;
+            res.status(200).json({message: `Usuário ${name} autorizado`});
         } catch (error) {
             console.log(error)
+        return res.status(error.status || 401).json({ error: error.message || 'Unauthorized'})
         }
-        
-        res.status(200).json({message: 'Session created successfully!'});
     }
 }
 
 module.exports = AuthController;
+
