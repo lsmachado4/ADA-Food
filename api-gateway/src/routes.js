9f6f5e819710680169fec4c3a33d7d7e25b89ebb
Thauan Fonseca <thauanfonseca20@gmail.com>
2024-02-24 15:44:25 -0300

    refactor: resolving conflicts

--- api-gateway/src/routes.js
+++ api-gateway/src/routes.js
@@ -4,6 +4,10 @@ const AddressHandler = require('./handlers/address-handler')
 const routes = new Router()
 
 routes.post('/session', SessionHandler.create)
+
+// Rotas Autenticadas (Precisa de token)
+routes.use(authMiddleware)
+routes.get('/auth', AuthHandler.auth)
 routes.post('/address', AddressHandler.address)
 
 
