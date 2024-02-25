b1b77b60d7c28654325b436fc913b997403912aa
laurienyestevo <lauryyestevo@gmail.com>
2024-02-24 22:36:19 -0300

    criando pedido

--- order-service/src/models/order.js
+++ order-service/src/models/order.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 
 const OrderSchema = mongoose.Schema({
-    id: String,
+    order_id: String,
     user_id: String,
     description: String,
 })
-
 module.exports = mongoose.model('Order', OrderSchema)
