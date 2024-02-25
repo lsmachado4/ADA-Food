c2726683240cc5d3c86ded3645bc3f51cb1bc3dc
Thauan Fonseca <thauanfonseca20@gmail.com>
2024-02-24 21:58:37 -0300

    feat: edit register-service

--- api-gateway/src/routes.js
+++ api-gateway/src/routes.js
@@ -1,10 +1,13 @@
 const {Router} = require('express')
 const SessionHandler = require('./handlers/session-handler')
+const RegisterHandler = require('./handlers/register-handler')
 const AddressHandler = require('./handlers/address-handler')
 const routes = new Router()
 
 routes.post('/session', SessionHandler.create)
 routes.post('/address', AddressHandler.address)
+
+routes.post('/register', RegisterHandler.create)
 
 
 module.exports = routes
